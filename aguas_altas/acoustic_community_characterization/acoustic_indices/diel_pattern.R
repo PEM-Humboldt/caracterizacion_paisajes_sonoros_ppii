28e109ea16ee8f4e52be60e010a82f2915a923fe
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2022-09-14 14:53:05 -0500

    Remove duplicated directory

--- aguas_altas/acoustic_community_characterization/acoustic_indices/diel_pattern.R
+++
@@ -1,7 +0,0 @@
-## COMPUTE MEAN DIEL PATTERN FOR EACH SITE
-setwd('/Volumes/lacie_macosx/Dropbox/PostDoc/iavh/ANH-PPII/analisis_fase_2/scripts/acoustic_community_caracterization/acoustic_indices/')
-df = read.csv('./dataframes/G010_indices.csv')
-
-df['time'] = factor(substr(df$date, 11,16))
-df_mean = aggregate(df$BI, by = list(df$time), FUN=mean)
-plot(df_mean)
